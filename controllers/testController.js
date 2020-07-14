b626b839dff19b78f6d925a345c28fe4522375df
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-07-14 19:36:24 -0400

    initial skeleton built

--- controllers/testController.js
+++ controllers/testController.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const test = require("../models/test");
+
 router.get("/", (req,res)=> {
+    test.all();
     res.send("Hello World!")
 });
 
